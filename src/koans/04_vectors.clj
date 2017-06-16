ec029e8cf89c422487feccc6b0d567a119d354e2
Julie Murakami <jmurakami@g.ucla.edu>
2017-06-16 01:49:33 -0400

    Update 04_vectors.clj

--- src/koans/04_vectors.clj
+++ src/koans/04_vectors.clj
@@ -5,12 +5,6 @@
   "You can use vectors in clojure as array-like structures"
   (= __ (count [42]))
 
-  "You can create a vector from a list"
-  (= __ (vec '(1)))
-
-  "Or from some elements"
-  (= __ (vector nil nil))
-
   "But you can populate it with any number of elements at once"
   (= [1 __] (vec '(1 2)))
 
@@ -25,9 +19,6 @@
 
   "Or any index if you wish"
   (= __ (nth [:peanut :butter :and :jelly] 3))
-
-  "You can also slice a vector"
-  (= __ (subvec [:peanut :butter :and :jelly] 1 3))
 
   "Equality with collections is in terms of values"
   (= (list 1 2 3) (vector 1 2 __)))
