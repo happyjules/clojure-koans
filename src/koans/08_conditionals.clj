8a945076f938b94a2287fca3f945b05b0a4f51d5
Julie Murakami <jmurakami@g.ucla.edu>
2017-06-16 01:52:42 -0400

    Update 08_conditionals.clj

--- src/koans/08_conditionals.clj
+++ src/koans/08_conditionals.clj
@@ -1,13 +1,6 @@
 (ns koans.08-conditionals
   (:require [koan-engine.core :refer :all]))
 
-(defn explain-exercise-velocity [exercise-term]
-  (case exercise-term
-        :bicycling        "pretty fast"
-        :jogging          "not super fast"
-        :walking          "not fast at all"
-        "is that even exercise?"))
-
 (meditations
   "You will face many decisions"
   (= __ (if (false? (= 4 5))
@@ -27,21 +20,7 @@
               :doom
               __))
 
-  "You may have a multitude of possible paths"
-  (let [x 5]
-    (= :your-road (cond (= x __) :road-not-taken
-                        (= x __) :another-road-not-taken
-                        :else __)))
-
   "Or your fate may be sealed"
   (= 'doom (if-not (zero? __)
           'doom
-          'more-doom))
-
-  "In case of emergency, go fast"
-  (= "pretty fast"
-     (explain-exercise-velocity __))
-
-  "But admit it when you don't know what to do"
-  (= __
-     (explain-exercise-velocity :watching-tv)))
+          'more-doom)))
